ef7081e9877ce249f7d8dfc8d5d6c478c8c385fd
Gui-developer <56438915+Gui-developer@users.noreply.github.com>
2020-03-05 11:01:03 -0300

    Delete Alter-drop-add-modify-insert.sql

--- Alter-drop-add-modify-insert.sql
+++
@@ -1,9 +0,0 @@
--- DDL
--- (ALTER - ADD/MODIFY)
-
-ALTER TABLE alunos
-ADD sexo CHAR(1) -- m ou f     -- ADICIONANDO TABELAS
-AFTER nome;
-
-ALTER TABLE alunos
-MODIFY sexo CHAR(1) NOT NULL;  -- alterando caracteristicas na tabela
